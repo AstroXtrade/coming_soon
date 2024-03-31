0348333f7afa97237f9feb25eb38fb326c75c99d
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2024-03-31 23:31:56 +0800

    chore: add color styles and padding styles to tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#4C37F4",
+        alt_primary: "#249CF3",
+        aux: "#F0A9F9",
+        tc: {
+          text_dark: "#393939",
+          text_gray: "#AEAEAE",
+        },
+        pc: {
+          light: "#FFFFFF",
+          almost_white: "#F8FAFC",
+          light_gray: "#FAFAFA",
+          dark: "#060606",
+          light_dark: "#2B2D3A",
+        },
+        status: {
+          success: {
+            "400": "#17E4A1",
+          },
+          warning: {
+            "400": "#FDC360",
+          },
+        },
+      },
+      container: {
+        // center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
       },
     },
   },
