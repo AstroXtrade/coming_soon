0b7bafecd641f4f7eccc4a144f782f7d39456610
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2024-03-31 23:56:57 +0800

    create a folder and default export for images and icons so they can easily be imported and utilized in different components through out the project

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,29 @@
+import LogoMark from "../../../public/icons/logo_mark.svg";
+import LogoTextWhite from "../../../public/icons/logo_type_white.svg";
+import LogoTextBlack from "../../../public/icons/logo_type_black.svg";
+import DropdownWhite from "../../../public/icons/dropdown.svg";
+import DropdownBlack from "../../../public/icons/dropdown_black.svg";
+import Sun from "../../../public/icons/sun.svg";
+import Moon from "../../../public/icons/moon.svg";
+import Arrow from "../../../public/icons/arrow.svg";
+
+// socials icon
+import Vk from "../../../public/icons/vk.svg";
+import X from "../../../public/icons/x.svg";
+import Instagram from "../../../public/icons/instagram-solid.svg";
+
+const Icon = {
+  LogoMark,
+  LogoTextBlack,
+  LogoTextWhite,
+  DropdownBlack,
+  DropdownWhite,
+  Sun,
+  Moon,
+  Arrow,
+  Vk,
+  X,
+  Instagram,
+};
+
+export default Icon;
---
+++ src/components/images/index.tsx
@@ -0,0 +1,9 @@
+import HomePageImageLight from "../../../public/image/image_light_mode.png";
+import HomePageImageDark from "../../../public/image/image_dark_mode.png";
+
+const Image = {
+  HomePageImageDark,
+  HomePageImageLight,
+};
+
+export default Image;
